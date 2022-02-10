0864e43fee6a71790b8552c7f7f57185935df72c
Lucas Ferreira <Lucasf538@gmail.com>
2022-02-10 14:59:57 -0300

    Projeto iniciado, recebendo dados bancarios, depositando e sacando sem impedimentos.

---
+++ ContaBancaria/Conta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaBancaria
+{
+    class Conta
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get; private set; }
+
+
+        public Conta(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.00;
+        }
+        public Conta(int numero, string titular, double saldo): this( numero, titular)
+        {
+            Saldo = saldo;
+
+        }
+
+        public void Deposito(double saldo )
+        {
+            Saldo += saldo;
+
+        }
+        public void Saque(double saque)
+        {
+            Saldo -= saque + 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular " + Titular + " Saldo: $" + Saldo;
+        }
+
+        
+
+
+
+
+
+
+
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+
+            Console.Write("Entre com o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Deseja iniciar com deposito inicial? s/n? ");
+            char r = char.Parse(Console.ReadLine());
+            if (r == 's'|| r =='S')
+            {
+                Console.Write("Entre com o valor do deposito inicial: ");
+                double Di = double.Parse(Console.ReadLine());
+                conta = new Conta(numero, titular,Di);
+                Console.WriteLine("Conta criada: " + conta);
+            }
+            else
+            {
+                conta = new Conta(numero, titular);
+                Console.WriteLine("Conta criada: "+conta);
+            }
+
+
+            Console.WriteLine("Deseja fazer um deposito? s/n ");
+            char d = char.Parse(Console.ReadLine());
+            while(d == 's'|| d == 'S' )
+            {
+                Console.WriteLine();
+                Console.Write("Entre com o valor do deposito: ");
+                double saldo = double.Parse(Console.ReadLine());
+                conta.Deposito(saldo);
+                Console.WriteLine();
+                Console.WriteLine("Conta Atualizada: " + conta);
+                Console.WriteLine();
+                Console.Write("Deseja depositar novamente? s/n ");
+                d = char.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Dseja fazer um saque? s/n ");
+            char s = char.Parse(Console.ReadLine());
+            while (s == 's' || s == 'S')
+            {
+                Console.Write("Entre com o valor para saque: ");
+                double saque = double.Parse(Console.ReadLine());
+                conta.Saque(saque);
+                Console.WriteLine();
+                Console.WriteLine("Conta Atualizada: " + conta);
+                Console.WriteLine();
+                Console.Write("Deseja sacar novamente? s/n ");
+                s = char.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("programa finalizado obrigado!! ");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
